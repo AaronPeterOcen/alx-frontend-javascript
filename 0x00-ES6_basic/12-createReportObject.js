d7015478f25dd90634cc71f01713f99ee94dbd67
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-23 02:39:13 +0300

    task 100 update

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allemployees: {
+    allEmployees: {
       ...employeesList,
     },
     getNumberOfDepartments(employeesList) {
