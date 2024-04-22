b3e2037dccda99fb1e9061e3b79f513fb4ac3400
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-23 01:58:30 +0300

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array1 = array;
-  for (let val of array) {
+  for (const val of array) {
     const idx = array.indexOf(val);
     array1[idx] = appendString + val;
   }
