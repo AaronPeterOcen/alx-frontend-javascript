f1ef2acb2cb0bf071850fd114953c1d121b63451
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-23 02:24:40 +0300

    task 3 update

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,7 @@
 export default function getSumOfHoods(
-  initialNumber = undefined,
+  initialNumber,
   expansion1989 = 89,
-  expansion2019 = 19,
+  expansion2019 = 19
 ) {
   if (expansion1989 !== undefined && expansion2019 !== undefined) {
     return initialNumber + expansion1989 + expansion2019;
