ed44b3b18beb65a5c9398cf4e3a12fabbe37dbf8
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 01:18:00 +0300

    task 3 currency class

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -30,21 +30,26 @@
 
 // console.log(initializeRooms());
 
-import HolbertonCourse from "./2-hbtn_course";
+// import HolbertonCourse from "./2-hbtn_course";
 
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
-console.log(c1.name);
-c1.name = "Python 101";
-console.log(c1);
+// const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
+// console.log(c1.name);
+// c1.name = "Python 101";
+// console.log(c1);
 
-try {
-  c1.name = 12;
-} catch (err) {
-  console.log(err);
-}
+// try {
+//   c1.name = 12;
+// } catch (err) {
+//   console.log(err);
+// }
 
-try {
-  const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-} catch (err) {
-  console.log(err);
-}
+// try {
+//   const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+// } catch (err) {
+//   console.log(err);
+// }
+
+import Currency from "./3-currency";
+
+const dollar = new Currency("$", "Dollars");
+console.log(dollar.displayFullCurrency());
