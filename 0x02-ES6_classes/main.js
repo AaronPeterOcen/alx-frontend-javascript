b000569a3fabc065ec678c7681eb191c13abe202
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-25 21:50:57 +0300

    task 11 evcar class

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,14 +1,22 @@
-import Car from "./10-car";
+// import Car from "./10-car";
 
-class TestCar extends Car {}
+// class TestCar extends Car {}
 
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
+// const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+// const tc2 = tc1.cloneCar();
 
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
+// console.log(tc1);
+// console.log(tc1 instanceof TestCar);
 
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
+// console.log(tc2);
+// console.log(tc2 instanceof TestCar);
 
-console.log(tc1 === tc2);
+// console.log(tc1 === tc2);
+
+import EVCar from './100-evcar';
+
+const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);
