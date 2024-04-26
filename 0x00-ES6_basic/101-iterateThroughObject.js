5ebc5d6e9c9c8b321a0fa243f03a3b37911134e6
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 22:53:05 +0300

    task 11

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,19 @@
+// export default function iterateThroughObject(reportWithIterator) {
+//   let employeeNames = '';
+
+//   for (const a of reportWithIterator) {
+//     for (const employee of a) {
+//       employeeNames += `${employee} | `;
+//     }
+//   }
+
+//   employeeNames = employeeNames.slice(0, -2);
+//   return employeeNames;
+// }
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = '';
-
-  for (const a of reportWithIterator) {
-    for (const employee of a) {
-      employeeNames += `${employee} | `;
-    }
+  let employees = '';
+  for (const item of reportWithIterator) {
+    employees += `${item} | `;
   }
-
-  employeeNames = employeeNames.slice(0, -2);
-  return employeeNames;
+  return employees.slice(0, -3);
 }
