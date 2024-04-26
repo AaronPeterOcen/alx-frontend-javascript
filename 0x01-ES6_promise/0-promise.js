516dbf4f9af35fecdea281ea7a3e19a422ba07f7
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 21:44:24 +0300

    update

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // API call logic here
+    // ...
+    if (responseFromAPI) {
+      resolve(responseFromAPI); // resolve with API response
+    } else {
+      reject('Error: API call failed'); // reject with error message
+    }
+  });
+}
