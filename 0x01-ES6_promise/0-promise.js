505f0d5c8bcc164948fc95b26705b468449b92e7
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 22:11:48 +0300

    update on promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,13 @@
-export default function getResponseFromAPI() {
+function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     // API call logic here
     // ...
     if (responseFromAPI) {
       resolve(responseFromAPI); // resolve with API response
     } else {
-      reject('Error: API call failed'); // reject with error message
+      reject("Error: API call failed"); // reject with error message
     }
   });
 }
+
+export default getResponseFromAPI;
