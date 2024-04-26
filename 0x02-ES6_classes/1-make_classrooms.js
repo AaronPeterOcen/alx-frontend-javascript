e59a9bd5adfb2b5926d0ab9a015c3b01a4098702
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 23:20:53 +0300

    update task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,5 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
-// class initializeRooms extends ClassRoom {
-//     constructor ()
-// }
 export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
