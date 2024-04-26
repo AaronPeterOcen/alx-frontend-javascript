ccf95ea79ac882b3cdc25531ae0c791ac1a052c8
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 22:41:55 +0300

    update task 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,19 +1,7 @@
-// export default function getSumOfHoods(
-//   initialNumber,
-//   expansion1989 = 89,
-//   expansion2019 = 19
-// ) {
-//   if (expansion1989 !== undefined && expansion2019 !== undefined) {
-//     return initialNumber + expansion1989 + expansion2019;
-//   }
-
-//   return getSumOfHoods;
-// }
-
 export default function getSumOfHoods(
   initialNumber,
   expansion1989 = 89,
-  expansion2019 = 19,
+  expansion2019 = 19
 ) {
   return initialNumber + expansion1989 + expansion2019;
 }
