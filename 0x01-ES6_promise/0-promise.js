da60402531c23ba15daae5a20080f57669ced79e
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-26 22:13:40 +0300

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,17 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // API call logic here
-    // ...
-    if (responseFromAPI) {
-      resolve(responseFromAPI); // resolve with API response
-    } else {
-      reject("Error: API call failed"); // reject with error message
-    }
-  });
-}
+// function getResponseFromAPI() {
+//   return new Promise((resolve, reject) => {
+//     // API call logic here
+//     // ...
+//     if (responseFromAPI) {
+//       resolve(responseFromAPI); // resolve with API response
+//     } else {
+//       reject("Error: API call failed"); // reject with error message
+//     }
+//   });
+// }
 
-export default getResponseFromAPI;
+// export default getResponseFromAPI;
+export default async function getResponseFromAPI() {
+  const promise = new Promise(() => {});
+  return promise;
+}
