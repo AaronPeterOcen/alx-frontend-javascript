008160af075995a240427580078fb6dda0bfad59
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-30 23:24:19 +0300

    update task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,24 @@
+export default function getListStudents() {
+  // let firstName = firstName;
+  // let id = id;
+  // let location = location;
+
+  const list = [
+    {
+      id: 1,
+      firstName: "Guillaume",
+      location: "San Francisco",
+    },
+    {
+      id: 2,
+      firstName: "James",
+      location: "Columbia",
+    },
+    {
+      id: 5,
+      firstName: "Serena",
+      location: "San Francisco",
+    },
+  ];
+  return list;
+}
