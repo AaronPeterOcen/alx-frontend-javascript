c13767b4aaafa5e1042bc72342e227d21879a1db
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-05-01 00:41:14 +0300

    task 5 returns an arraybuffer with int8 value

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error("Position outside range");
+  }
   const arr = new ArrayBuffer(length);
   const val = new DataView(arr);
+  val.setInt8(position, value);
 
-  try {
-    val.setInt8(position, value);
-  } catch (e) {
-    throw Error("Position outside range");
-  }
   return val;
 }
