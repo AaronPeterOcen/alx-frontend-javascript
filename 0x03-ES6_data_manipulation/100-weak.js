42d22475a4814f62b9e230aa719cf76f9ca877b2
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-05-01 00:52:22 +0300

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  // if (!weakMap.has(endpoint)) {
-  //   weakMap.set(endpoint, 0);
-  // }
-  // weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  // if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-  //   throw new Error("Endpoint load is high");
-  // }
-  let called = weakMap.get(endpoint) || 0;
-
-  called += 1;
-
-  weakMap.set(endpoint, called);
-
-  if (called >= 5) {
-    throw Error("Endpoint load is high");
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
-  return called;
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error("Endpoint load is high");
+  }
 }
