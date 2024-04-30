1dab2257fe58c3bfdc17be70dc92b095550ddfe8
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-30 19:58:36 +0300

    update

---
+++ 0x03-ES6_data_manipulation/main copy.js
@@ -0,0 +1,88 @@
+import getListStudents from './0-get_list_students';
+
+console.log(getListStudents());
+
+// // 1
+// import getListStudentIds from "./1-get_list_student_ids";
+// import getListStudents from "./0-get_list_students";
+
+// console.log(getListStudentIds("hello"));
+// console.log(getListStudentIds(getListStudents()));
+
+// // 2
+// // import getListStudents from "./0-get_list_students";
+// // import getStudentsByLocation from "./2-get_students_by_loc";
+
+// // const students = getListStudents();
+
+// // console.log(getStudentsByLocation(students, "San Francisco"));
+
+// //3
+// import getListStudents from "./0-get_list_students";
+// import getStudentIdsSum from "./3-get_ids_sum";
+
+// const students = getListStudents();
+// const value = getStudentIdsSum(students);
+
+// console.log(value);
+
+// // 4
+// import getListStudents from "./0-get_list_students";
+// import updateStudentGradeByCity from "./4-update_grade_by_city";
+
+// console.log(
+//   updateStudentGradeByCity(getListStudents(), "San Francisco", [
+//     { studentId: 5, grade: 97 },
+//     { studentId: 1, grade: 86 },
+//   ])
+// );
+
+// console.log(
+//   updateStudentGradeByCity(getListStudents(), "San Francisco", [
+//     { studentId: 5, grade: 97 },
+//   ])
+// );
+
+// // 5
+// import createInt8TypedArray from "./5-typed_arrays";
+
+// console.log(createInt8TypedArray(10, 2, 89));
+
+// // 6
+// import setFromArray from "./6-set";
+
+// console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+
+// // 7
+// import hasValuesFromArray from "./7-has_array_values";
+
+// console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+// console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+// console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+
+// // 8
+// import cleanSet from "./8-clean_set";
+
+// console.log(
+//   cleanSet(new Set(["bonjovi", "bonaparte", "bonappetit", "banana"]), "bon")
+// );
+// console.log(
+//   cleanSet(new Set(["bonjovi", "bonaparte", "bonappetit", "banana"]), "")
+// );
+
+// // 9
+// import groceriesList from "./9-groceries_list";
+
+// console.log(groceriesList());
+
+// // 10
+// import updateUniqueItems from "./10-update_uniq_items";
+// import groceriesList from "./9-groceries_list";
+
+// const map = groceriesList();
+// console.log(map);
+
+// updateUniqueItems(map);
+// console.log(map);
+
+// // 11
