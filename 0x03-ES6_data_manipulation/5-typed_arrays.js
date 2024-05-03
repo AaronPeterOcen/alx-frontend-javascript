8dcbc6d2bcc9379665ef6ce9513cef26c3f73e0a
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-05-04 00:30:44 +0300

    upd

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,6 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
   const arr = new ArrayBuffer(length);
   const val = new DataView(arr);
   val.setInt8(position, value);
-
   return val;
 }
